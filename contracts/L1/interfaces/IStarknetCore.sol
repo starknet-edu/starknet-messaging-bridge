908d8592c7421a87b1a9e1203985b683900acd71
Othman Sefraoui <sefraouiothman7@gmail.com>
2022-02-08 23:38:26 +0100

    L1 and L2 contracts for the first part

---
+++ contracts/L1/interfaces/IStarknetCore.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+interface IStarknetCore {
+    /**
+      Sends a message to an L2 contract.
+
+      Returns the hash of the message.
+    */
+    function sendMessageToL2(
+        uint256 to_address,
+        uint256 selector,
+        uint256[] calldata payload
+    ) external returns (bytes32);
+
+    /**
+      Consumes a message that was sent from an L2 contract.
+
+      Returns the hash of the message.
+    */
+    function consumeMessageFromL2(
+        uint256 fromAddress,
+        uint256[] calldata payload
+    ) external returns (bytes32);
+}
